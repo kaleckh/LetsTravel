6d0b4635a42e909c0aec8904652e49f236fa050b
Kaleck Hamm <kaleckh@gmail.com>
2022-11-28 16:55:49 -0700

    login more functional, showing register inputs

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,25 +1,101 @@
-import React, { Component } from "react";
-import "./Login.css"
+import React, { useEffect, useState } from "react";
+import "./Login.css";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../Firebase";
+function Login() {
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({});
+  const [createAccount, setCreateAccount] = useState(false);
 
-class Login extends Component{
-    constructor(props) {
-        super(props)
+  useEffect(() => {
+    if (auth?.currentUser?.email) {
+      onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+      });
     }
+  }, [auth?.currentUser?.email]);
 
-    render() {
-        return(
-            <div className="container">
-                <div className="box">
-                    <input className="loginInput" type="text" name="" id="" />
-                    <input className="loginInput" type="text" />
-                    <button className="">Login</button>
-                </div>
-            </div>
-        )
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <div>
+      {!createAccount ? (
+        <div className="container">
+          <div className="box">
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Email"
+              onChange={(event) => {
+                setLoginEmail(event.target.value);
+              }}
+            />
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Password"
+              onChange={(event) => {
+                setLoginPassword(event.target.value);
+              }}
+            />
+            <button onClick={register} className="">
+              Login
+            </button>
+            <button
+              onClick={() => {
+                setCreateAccount(!createAccount);
+                
+              }}
+            >
+              Create Account
+            </button>
+            {/* {user?.currentUser} */}
+          </div>
+        </div>
+      ) : (
+        <div className="container">
+          <div className="box">
+            <input type="text" />
+            <input type="text" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-
-export default Login
+export default Login;
